04cf0cb02ccd0c02829e8b0ddbc37e5e8df2c1d5
Dmitriy <dkom91@gmail.com>
2019-05-08 12:58:10 +0300

    <master/task_001> создание основной ветки проекта
    - добавлен main класс для старта приложения
    - добавлены маппинги на страницы greeting и main

---
+++ src/main/java/dk/project/sweter/Application.java
@@ -0,0 +1,19 @@
+package dk.project.sweter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * класс запуска приложения
+ * поднимает спринг контекст
+ *
+ * @author dkom91
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
